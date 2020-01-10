11c33cf0ac3e32831e0dde6ecdd94eab8bf9eb0b
Trinity Terry <trinitycterry@gmail.com>
2020-01-10 13:41:36 -0600

    Finished directions

---
+++ src/scripts/journal.js
@@ -0,0 +1,12 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represent
+    s a journal entry about what you learned today
+*/
+const journalEntry = {
+
+};
+
+let journalCollection = [];
+
+journalCollection.push(journalEntry);
