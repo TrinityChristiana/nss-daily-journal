018c8fbd31cffd0f9968506d46df0e25333cbb66
Trinity Terry <trinitycterry@gmail.com>
2020-01-21 13:47:27 -0600

    deleted unnecessary comment

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,19 +1,4 @@
 // Move the code that deals with getting the data into this file.
-
-// fetch("http://localhost:8088/journalEntries") // Fetch from the API
-//     .then(response => response.json())  // Parse as JSON
-//     .then(entries => {
-//         // What should happen when we finally have the array?
-//         entries.forEach(entry => {
-//             // Creates Empty array to hold journalEntry's html text
-//             let journalEntryHTML = [];
-//             // Pushes returned value from makeJournalEntryComponent for each entry to journaEntryHTML array.
-//             journalEntryHTML.push(makeJournalEntryComponent(entry));
-//             // Renders journal entry for each entry
-//             renderJournalEntries(journalEntryHTML);
-//         });
-//     });
-
 const API = {
     getJournalEntries () {
         return fetch("http://localhost:8088/journalEntries")
