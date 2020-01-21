d2f1dc997be10002bca3e722710a4e381a6b8b0a
Trinity Terry <trinitycterry@gmail.com>
2020-01-21 13:43:25 -0600

    Added entryComponent file to main

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,15 +1,3 @@
-// API.then(entries => {
-//     // What should happen when we finally have the array?
-//     entries.forEach(entry => {
-//         // Creates Empty array to hold journalEntry's html text
-//         let journalEntryHTML = [];
-//         // Pushes returned value from makeJournalEntryComponent for each entry to journaEntryHTML array.
-//         journalEntryHTML.push(makeJournalEntryComponent(entry));
-//         // Renders journal entry for each entry
-//         renderJournalEntries(journalEntryHTML);
-//     });
-// })
-
 /*
     Main application logic that uses the functions and objects
     defined in the other JavaScript files.
@@ -17,7 +5,7 @@
     Change the fake variable names below to what they should be
     to get the data and display it.
 */
-// objectWithGetterMethod.methodToGetData().then(renderJournalEntries(null));
+import entryComponent from "./entryComponent.js"
 API.getJournalEntries().then(function(entries) {
     let component = [];
     entries.forEach(entry => {
